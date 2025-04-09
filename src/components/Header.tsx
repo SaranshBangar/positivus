dd518dda6fa58be27c48f043e8c248130c5df48c
Saransh Bangar <saranshbangad@gmail.com>
2025-04-09 22:07:46 +0530

    Add new dependencies, update styles, and implement responsive header with sheet component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,7 @@
+import { Menu } from "lucide-react";
 import LogoWithName from "./helper/LogoWithName";
+
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 
 const HeaderLinks = [
   { name: "About us", path: "/" },
@@ -10,11 +13,12 @@ const HeaderLinks = [
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-between mt-[60px] mb-[70px] px-[100px]">
+    <header className="flex items-center justify-between md:mt-[60px] md:mb-[70px] md:px-[100px] px-5 pt-[30px] pb-[40px]">
       <div>
         <LogoWithName />
       </div>
-      <div className="flex items-center gap-10">
+
+      <div className="hidden items-center gap-10 md:flex">
         <ul className="flex gap-10">
           {HeaderLinks.map((link) => (
             <a key={link.name} href={link.path}>
@@ -22,9 +26,35 @@ const Header = () => {
             </a>
           ))}
         </ul>
-        <button className="rounded-[14px] border-[var(--color-dark)] border-[1px] py-5 px-[35px]">
+        <button className="rounded-[14px] border-[var(--color-dark)] border-[1px] py-5 px-[35px] hover:bg-accent transition-all">
           <p>Request a quote</p>
         </button>
+      </div>
+
+      <div className="block md:hidden">
+        <Sheet>
+          <SheetTrigger>
+            <Menu />
+          </SheetTrigger>
+          <SheetContent side="top" className="pt-10 border-b-2 border-accent">
+            <nav>
+              <ul className="flex flex-col gap-5">
+                {HeaderLinks.map((link) => (
+                  <li key={link.name} className="text-center">
+                    <a href={link.path}>
+                      <p>{link.name}</p>
+                    </a>
+                  </li>
+                ))}
+              </ul>
+              <div className="m-4">
+                <button className="w-full rounded-[14px] border-[var(--color-dark)] border-[1px] py-4 px-[35px]">
+                  <p>Request a quote</p>
+                </button>
+              </div>
+            </nav>
+          </SheetContent>
+        </Sheet>
       </div>
     </header>
   );
