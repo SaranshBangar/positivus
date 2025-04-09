1514cf6207b2bbf1c1b10679e804f8fc54db6a4c
Saransh Bangar <saranshbangad@gmail.com>
2025-04-09 22:26:09 +0530

    feat: enhance Hero component with responsive design and content
    - Updated Hero component to include a main section with two responsive layouts: one for larger screens and another for mobile.
    - Added a heading, descriptive paragraph, and a consultation button to the Hero section.
    - Included an SVG image of a speaker for visual appeal.
    - Improved styling for better user experience across different screen sizes.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,35 @@
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <main>
+      <section className="hidden md:flex flex-row gap-[108px]">
+        <div className="flex-1 flex flex-col gap-9 justify-center items-start px-[100px]">
+          <h1>Navigating the digital landscape for success</h1>
+          <p>
+            Our digital marketing agency helps businesses grow and succeed online through a range of services including SEO, PPC, social media
+            marketing, and content creation.
+          </p>
+          <button className="rounded-[14px] bg-[var(--color-dark)] py-5 px-[35px] text-[var(--color-light)]">
+            <p>Book a consultation</p>
+          </button>
+        </div>
+        <div className="flex-1">
+          <img src="/speaker.svg" alt="speaker" />
+        </div>
+      </section>
+
+      <section className="md:hidden flex flex-col gap-[10px] justify-center items-start w-full mx-5">
+        <h1>Navigating the digital landscape for success</h1>
+        <img src="/speaker.svg" alt="speaker" />
+        <p>
+          Our digital marketing agency helps businesses grow and succeed online through a range of services including SEO, PPC, social media
+          marketing, and content creation.
+        </p>
+        <button className="rounded-[14px] bg-[var(--color-dark)] py-5 px-[35px] text-[var(--color-light)] w-full">
+          <p>Book a consultation</p>
+        </button>
+      </section>
+    </main>
+  );
 };
 
 export default Hero;
